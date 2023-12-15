3842ad2538c4ce49c303b8b88b569af0ce0de83e
Me oof <133184687+OOFMAN29803@users.noreply.github.com>
2023-12-15 19:32:20 +1100

    V 0.3 Alpha
    Added:
    A new (janky) UI, instead of straight from console (main only).
    Some Error messages (No training file .txt, code problems).
    Some more placeholder .txt's.

--- aimain.py
+++ aimain.py
@@ -1,16 +1,48 @@
 import re
+import tkinter as tk
 
-for i in range(200):
+def sanitize_filename(filename):
+  filename = filename[:255]
+  sanitized_filename = re.sub(r'[<>:"/\\|?*.,]', '', filename)
+  return sanitized_filename
+def read_file():
+    try:
+      prompt = entry.get().lower()
+      prompt = prompt[:255]
+      sanitized_prompt = sanitize_filename(prompt)
+      with open(sanitized_prompt + ".txt", 'r') as file:
+        updated_content = file.read()
+        updated_content = updated_content[:255]
+    except FileNotFoundError:
+        error = "SilicoLM: I am sorry I do not understand, You may not have trained the AI to understand  >" + "'" + prompt + "'" + "<, try again once you have trained it."
+        listbox.insert(tk.END, "You: " + prompt)
+        listbox.insert(tk.END, "     ")
+        listbox.insert(tk.END, error)
+        listbox.insert(tk.END, "     ")
+    except Exception as e:
+        error = "SilicoLM: An Error Occurred, if this continues, post an issue in Github on the Issues page"
+        listbox.insert(tk.END, "You: " + prompt)
+        listbox.insert(tk.END, "     ")
+        listbox.insert(tk.END, error)
+        listbox.insert(tk.END, "     ")     
+    finally:
+            print("Successful Prompt with 0 known errors")
+            listbox.insert(tk.END, "You: " + prompt)
+            listbox.insert(tk.END, "     ")          
+            listbox.insert(tk.END,"SilicoLM: " + updated_content)
+            listbox.insert(tk.END, "     ")
+        
+root = tk.Tk()
+root.title("SilicoLM AI")
 
-    def sanitize_filename(filename):
 
-        sanitized_filename = re.sub(r'[<>:"/\\|?*.]', '', filename)
-        return sanitized_filename
+save_button = tk.Button(root, text="Enter", command=read_file)
+save_button.grid(row=2, column=1, padx=10, pady=10)
 
-prompt = input("Answer from the bot: ").lower()
+listbox = tk.Listbox(root, height=40, width=90)
+listbox.grid(row=1, column=0, columnspan=3, padx=10, pady=10)
 
-sanitized_prompt = sanitize_filename(prompt)
+entry = tk.Entry(root, width=30)
+entry.grid(row=2, column=0, padx=2, pady=2)
 
-with open(sanitized_prompt + ".txt",'r') as file:
-    updated_content = file.read()
-    print(updated_content)
+root.mainloop()
--- aitrain.py
+++ aitrain.py
@@ -1,18 +1,17 @@
 import re
 
 for i in range(200):
-    
-    def sanitize_filename(filename):
 
-        sanitized_filename = re.sub(r'[<>:"/\\|?*.]', '', filename)
-        return sanitized_filename
+  def sanitize_filename(filename):
+    filename = filename[:255]
+    sanitized_filename = re.sub(r'[<>:"/\\|?*.,]', '', filename)
+    return sanitized_filename
+    prompt = input("Input desired prompt for AI: ").lower()
+    answer = input("Input desired answer for AI: ")
+    sanitized_prompt = sanitize_filename(prompt)
+    prompt = prompt[:255]
 
-prompt = input("Input desired prompt for AI: ").lower()
-answer = input("Input  desired answer for AI: ")
-
-sanitized_prompt = sanitize_filename(prompt)
-
-
-with open(sanitized_prompt + '.txt','w') as file:
-    file.write(answer)
+    with open(sanitized_prompt + '.txt','w') as file:
+      file.write(answer)
+      sanitize_filename(filename)
 
